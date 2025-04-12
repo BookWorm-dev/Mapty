a6d178fd952f58898664c80b7101a189e2305f41
Kristina <kgritsenko.2017@gmail.com>
2025-04-12 10:52:17 +0300

    feat: move to marker on click

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class Workout {
     date = new Date();
     id = (Date.now() + '').slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration) {
         this.coords = coords; //[lat, lng]
@@ -27,6 +28,10 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+
+    }
+    click() {
+        this.clicks++;
     }
 }
 
@@ -62,10 +67,9 @@ class Cycling extends Workout {
     }
 }
 
-///////////////////////////////////////////////
-//Application Arhitecture
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -73,6 +77,7 @@ class App {
         this._getPosition();
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElevationField);
+        containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
     }
 
     _getPosition() {
@@ -89,7 +94,7 @@ class App {
         const coords = [latitude, longitude];
         console.log(latitude, longitude);
 
-        this.#map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -233,8 +238,26 @@ class App {
 
     }
 
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest('.workout');
+
+        if (!workoutEl) return;
+
+        const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
+        console.log(workout);
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1
+            }
+        })
+        //using the public interface
+
+        workout.click();
+
+    }
+
 }
 
-
 const app = new App();
-
